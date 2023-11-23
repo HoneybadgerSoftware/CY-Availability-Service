45c39257107a26dc06cfb5ac31d183d39d9cb599
Jakub <jakub.kolacz.dev@gmail.com>
2023-11-23 19:07:41 +0100

    checkstyle fixes

--- src/main/java/com/honeybadgersoftware/availability/utils/ManyToOneFactory.java
+++ src/main/java/com/honeybadgersoftware/availability/utils/ManyToOneFactory.java
@@ -3,11 +3,11 @@ package com.honeybadgersoftware.availability.utils;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public interface ManyToOneFactory <T,R,S>{
+public interface ManyToOneFactory<T, R, S> {
 
-    S map (T t,R r);
+    S map(T t, R r);
 
-    default List<S> map(List<T> t, R r){
+    default List<S> map(List<T> t, R r) {
         return t.stream().map(o1 -> this.map(o1, r)).collect(Collectors.toList());
     }
 }
