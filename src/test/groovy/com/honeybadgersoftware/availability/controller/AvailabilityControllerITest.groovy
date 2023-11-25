c83746fc89871f92e5bdc698be1affa8cf9b8ef7
Jakub <jakub.kolacz.dev@gmail.com>
2023-11-26 00:24:17 +0100

    finished test //todo code refactor

--- src/test/groovy/com/honeybadgersoftware/availability/controller/AvailabilityControllerITest.groovy
+++ src/test/groovy/com/honeybadgersoftware/availability/controller/AvailabilityControllerITest.groovy
@@ -6,7 +6,11 @@ import com.honeybadgersoftware.availability.model.UpdateAvailabilityData
 import com.honeybadgersoftware.availability.model.UpdateAvailabilityRequest
 import com.honeybadgersoftware.availability.repository.AvailabilityRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*
@@ -35,15 +39,26 @@ class AvailabilityControllerITest extends BaseIntegrationTest{
         UpdateAvailabilityData newProductData = new UpdateAvailabilityData(2L, new BigDecimal("29.99"))
         UpdateAvailabilityRequest request = new UpdateAvailabilityRequest(shopId, [existingProductData], [newProductData])
 
+        HttpHeaders headers = new HttpHeaders()
+        headers.setContentType(MediaType.APPLICATION_JSON)
+        HttpEntity<UpdateAvailabilityRequest> requestEntity = new HttpEntity<>(request, headers)
+
         when:
-        ResponseEntity<Void> response = restTemplate.put(addressToUseForTests + "/availability/update", request, Void.class)
+        ResponseEntity<Void> response = restTemplate.exchange(
+                addressToUseForTests + "/availability/update",
+                HttpMethod.PUT,
+                requestEntity,
+                Void.class)
 
         then:
-        response.statusCode == HttpStatus.OK
+        response.getStatusCode() == HttpStatus.OK
 
         and: "Check if products state changed"
         def productsAfterUpdate = availabilityRepository.findAllByProductIds([1L, 2L])
 
         productsBeforeUpdate != productsAfterUpdate
+
+        println(productsBeforeUpdate)
+        println(productsAfterUpdate)
     }
 }
