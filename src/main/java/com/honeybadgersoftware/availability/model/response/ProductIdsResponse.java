a3b329106494cd109700ac843677752586fc3ac9
Jakub <jakub.kolacz.dev@gmail.com>
2023-12-06 20:41:09 +0100

    create random prducts by shops functionality

--- src/main/java/com/honeybadgersoftware/availability/model/response/ProductIdsResponse.java
+++ src/main/java/com/honeybadgersoftware/availability/model/response/ProductIdsResponse.java
@@ -1,4 +1,4 @@
-package com.honeybadgersoftware.availability.model.request;
+package com.honeybadgersoftware.availability.model.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,6 +11,6 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class ProductIndexesRequest {
+public class ProductIdsResponse {
     private List<Long> data;
 }
